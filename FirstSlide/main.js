0367096b5fe70a0daeb998f1540971199876d471
AtikinXX <atikinxx@gmail.com>
2021-02-13 17:53:06 +0300

    4

--- FirstSlide/main.js
+++ FirstSlide/main.js
@@ -2,3 +2,10 @@
 function changeImage(anything){
   document.getElementById('slider').src = anything;
 }
+//ФУНКЦИЯ ОТКРЫТИЯ И ЗАКРЫТИЯ МЕНЮ
+function ToggleMenu(){
+  const menuToggle = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
